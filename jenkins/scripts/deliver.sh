c9dfd376778bee1b57b3538794fe01bad13f49ff
Krishna Kumar <krish_ashok66@yahoo.com>
2026-03-14 20:56:00 +0530

    updated script

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,13 +5,13 @@ echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 set -x
-mvn -f app/pom.xml jar:jar install:install help:evaluate -Dexpression=project.name
+mvn -f app/pom.xml jar:jar install:install help:evaluate -Dexpression=project.artifactId
 set +x
 
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn -f app/pom.xml -q -DforceStdout help:evaluate -Dexpression=project.name`
+NAME=`mvn -f app/pom.xml -q -DforceStdout help:evaluate -Dexpression=project.artifactId`
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
